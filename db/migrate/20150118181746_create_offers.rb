c2e9abd58700a2895d1f62298bc2907baf86ee3b
Ryan Brigden <rbrigden@marlow.andover.edu>
2015-01-23 19:01:49 -0500

    prior to schema

--- db/migrate/20150118181746_create_offers.rb
+++ db/migrate/20150118181746_create_offers.rb
@@ -1,6 +1,11 @@
 class CreateOffers < ActiveRecord::Migration
   def change
     create_table :offers do |t|
+    
+    	t.string :title
+    	t.string :first
+    	t.string :last
+    	t.integer :isbn
 
       t.timestamps null: false
     end
