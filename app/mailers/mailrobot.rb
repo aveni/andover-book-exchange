e41ff3e50061c9417b1a6858a283ca20b5e41350
Ryan Brigden <brigdenr@gmail.com>
2015-02-26 23:00:28 -0500

    deleted notify listing

--- app/mailers/mailrobot.rb
+++ app/mailers/mailrobot.rb
@@ -3,19 +3,6 @@ class Mailrobot < ApplicationMailer
 	default reply_to: "brigdenr@gmail.com"
 
 
-# def notify_listing(user, listing)
-		# @listing = listing
-		# @user = user
-		# if Rails.env.development?
-		# 	recipient = "brigdenr@gmail.com"
-		# else
-		# 	recipient = @user.email
-		# end
-		# mail(to:recipient, subject:"Listing for #{@listing.book.title} has been created")
-
-
-	# end
-
 	def notify_buyer(buyer, seller, listing)
 		@buyer = buyer
 		@seller = seller
