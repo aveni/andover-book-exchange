f0bc8d64884c71c02e2df4ac4d752b7de0da0eb0
Ryan Brigden <brigdenr@gmail.com>
2015-02-27 09:26:29 -0500

    finished all email templates and functionality. attachments for 2.0

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
+ActiveRecord::Schema.define(version: 20150225220855) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -65,13 +65,6 @@
     t.text     "text"
     t.integer  "user_id"
     t.integer  "book_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "transactions", force: :cascade do |t|
-    t.integer  "listing_id"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
